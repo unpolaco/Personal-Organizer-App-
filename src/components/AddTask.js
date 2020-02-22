747dd792ebace4a2604b50bd663707fd89a1b337
Michal Borowski <unpolacoo@gmail.com>
2020-02-22 23:20:45 +0100

    AddTask handlers

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 
 class AddTask extends Component {
   state = {
-
+    newTaskName: "",
+    newTaskDate: "",
+    newTaskPriority: false,
+    newTask: "",
     }
+
+    handleCheckPriority = () => {
+      this.setState({
+        newTaskPriority: !this.state.newTaskPriority,
+      })
+    }
+    handleChangeTaskName = (e) => {
+      this.setState({
+        newTaskName: e.target.value,
+      })
+    }
+    handleChangeDate = (e) => {
+      this.setState({
+        date: e.target.value,
+      })
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault()
+      const {newTaskName, newTaskDate, newTaskPriority} = this.state;
+      const newTask = {
+        name: newTaskName,
+        date: newTaskDate,
+        priority: newTaskPriority,
+        done: false,
+      }
+      this.setState({
+        newTask,
+      })
+      console.log(newTask);
+      
+    }
+
   render() { 
     return ( 
-      <p>Dodaj zadanie</p>
+      <form>
+        <input type="text" placeholder="Wpisz zadanie" onChange={this.handleChangeTaskName} />
+        <input type="date" value={this.state.date} onChange={this.handleChangeDate} />
+        <input type="checkbox" onChange={this.handleCheckPriority} />
+        <button onClick={this.handleSubmit} >Dodaj zadanie</button>
+      </form>
      );
   }
 }
