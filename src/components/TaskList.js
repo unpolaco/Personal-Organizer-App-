24ba68d5151ebe2468d84b1a1e9f46dc00742724
Michal Borowski <unpolacoo@gmail.com>
2020-02-22 21:28:25 +0100

    Rewrite TaskList Component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,36 +3,26 @@ import Task from './Task'
 
 function TaskList(props) {
   const {tasks, handleDeleteTask, handleDoneTask} = props;
+  
+  const activeList = tasks.filter(task => !task.done);
+  const activeTasks = activeList.map(task => <Task 
+                handleDeleteTask={handleDeleteTask} 
+                handleDoneTask={handleDoneTask} 
+                key={task.id} 
+                tasks={task}/>)
+  const doneList = tasks.filter(task => task.done);
+  const doneTasks = doneList.map(task => <Task 
+                handleDeleteTask={handleDeleteTask} 
+                handleDoneTask={handleDoneTask} 
+                key={task.id} 
+                tasks={task}/>)
   return (
     <div>
     <h2>Zadania do zrobienia</h2>
-
-      {tasks.map(task => {
-        return (
-          !task.done ? 
-          <Task 
-          handleDeleteTask={handleDeleteTask} 
-          handleDoneTask={handleDoneTask} 
-          key={task.id} 
-          tasks={task}/> 
-          : false
-        )
-      }
-      )}
+      {activeTasks}
+   
     <h2>Zadania zrobione</h2>
-
-    {tasks.map(task => {
-        return (
-          task.done ? 
-          <Task 
-          handleDeleteTask={handleDeleteTask} 
-          handleDoneTask={handleDoneTask} 
-          key={task.id} 
-          tasks={task}/>
-          : false
-        )
-      }
-      )}
+      {doneTasks}
     </div>
   )
 }
